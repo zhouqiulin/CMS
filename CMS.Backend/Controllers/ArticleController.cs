f0c4f9e60ae9ea4991221b9f03edd1dae805eca0
zhouqiulin <zql394@foxmail.com>
2017-02-07 22:40:37 +0800

    测试EF添加操作

--- CMS.Backend/Controllers/ArticleController.cs
+++ CMS.Backend/Controllers/ArticleController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CMS.Data;
+using CMS.Data.Model.Entity;
+using CMS.Data.Model.Enum;
+
+
 
 namespace CMS.Backend.Controllers
 {
     public class ArticleController : Controller
     {
-        public IActionResult Index()
+        private readonly CMSDBContext _context;
+
+
+        public ArticleController(CMSDBContext context) {
+            _context = context;
+        }
+        public IActionResult List()
         {
+
+
             return View();
         }
-       
+
+        public IActionResult Add()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Article o = new Article()
+                {
+                    Title = $"这是第{i}个标题",
+                    Summary= $"这是第{i}个描述信息",
+                    
+                };
+                _context.Article.Add(o);
+
+            }
+
+            _context.SaveChanges();
+
+            return null;
+        }
+
     }
 }
