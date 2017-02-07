bc6545ff78ebb8b50b20a078ea723cd8388378d5
zhouqiulin <zql394@foxmail.com>
2017-02-07 20:53:21 +0800

    添加EF

---
+++ CMS.Data/CMSDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CMS.Data.Model.Entity;
+
+namespace CMS.Data
+{
+    public class CMSDBContext:DbContext
+    {
+        DbSet<Article> Articles { get; set; }
+        DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ZQL-PC;Initial Catalog=CMS;Integrated Security=True");
+        }
+
+    }
+}
--- CMS.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CMS.Data
-{
-    public class Class1
-    {
-    }
-}
